4cc0442d430d37781ffa76318e3d4c4125cff620
benji703 <benjadcool@gmail.com>
2022-11-11 11:52:50 +0100

    most contributions python implementation

--- Backend_server/server.py
+++ Backend_server/server.py
@@ -14,9 +14,9 @@ locale.getpreferredencoding()
 
 spark = SparkSession.builder.appName('streamTest') \
     .config('spark.master','spark://spark-master:7077') \
-    .config('spark.executor.cores', 1) \
-    .config('spark.cores.max',1) \
-    .config('spark.executor.memory', '1g') \
+    .config('spark.executor.cores', 2) \
+    .config('spark.cores.max',2) \
+    .config('spark.executor.memory', '2g') \
     .config('spark.sql.streaming.checkpointLocation','hdfs://namenode:9000/stream-checkpoint/') \
     .getOrCreate()
 
@@ -57,6 +57,37 @@ def getLanguages():
     print(message)
     
     return message
+
+@app.route('/mostContributions')
+@cross_origin()
+def getMostContributions():
+    file = "hdfs://namenode:9000/commitData/commitAuthorRepo.json"
+    df = spark.read.json(file)
+    df.show()
+
+    name = request.args.get("reponame")
+
+    dataByName = df.filter(df.repo_name[0] == name)
+    #dataByName = dataByName.select("repo_name", "author")
+
+    dataByName.show()
+    authors = dataByName.select("author.name").collect()
+    print(authors)
+
+    authorDict = dict()
+    i = 0
+    while (i < len(authors)):
+        print(authors[i][0])
+        if (authorDict.get(authors[i][0]) != None):
+            count = authorDict.get(authors[i][0])
+            authorDict.update({authors[i][0]: count + 1})
+        else:
+            authorDict[authors[i][0]] = 1
+        i += 1
+    
+    max_key = max(authorDict, key=authorDict.get)
+    
+    return "Most contributions to " + name + " are made by: " + max_key + " with " + str(authorDict.get(max_key)) + " commits"
 
 @app.route('/person/', methods=['POST'])
 @cross_origin()
