41853a7f00ac8d5c632889e20caa293ebfcc8082
Andreas Edal Pedersen <anepe19@student.sdu.dk>
2022-11-11 08:47:06 +0100

    dotnet producer for kafka

---
+++ Prototypes/replay-past-data-into-kafka/dotnet-producer/KafkaDocker/Program.cs
@@ -0,0 +1,9 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "kafka:9092",
+};
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+var result = await producer.ProduceAsync("foobar", new Message<Null, string> {Value = "message"});
