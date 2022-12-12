a5e7489c7494dc116c78843cd4d9f76b927d4ea6
AndreasEdal <anepe19@student.sdu.dk>
2022-12-12 21:42:49 +0100

    Made sure that park job fails if no url has been added

--- SparkTransformation/commitsTransformation/sparkCommits.py
+++ SparkTransformation/commitsTransformation/sparkCommits.py
@@ -7,9 +7,9 @@ import os
 locale.getdefaultlocale()
 locale.getpreferredencoding()
 
-spark_master_url = os.environ["DAAB_SPARK_URL"] or "spark-master:7077"
-namenode_url = os.environ["DAAB_NAMENODE_URL"] or "namenode:9000"
-kafka_url = os.environ["DAAB_KAFKA_URL"] or "kafka:9092"
+spark_master_url = os.environ["DAAB_SPARK_URL"]
+namenode_url = os.environ["DAAB_NAMENODE_URL"]
+kafka_url = os.environ["DAAB_KAFKA_URL"]
 
 print("spark_master_url: " + spark_master_url)
 print("namenode_url: " + namenode_url)
