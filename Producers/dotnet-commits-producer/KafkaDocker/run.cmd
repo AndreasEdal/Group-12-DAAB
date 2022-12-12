227d1874ad0ec1569dd0c55dc0b974d088bd3fe1
AndreasEdal <anepe19@student.sdu.dk>
2022-12-12 20:25:12 +0100

    Removed producers again

--- Producers/dotnet-commits-producer/KafkaDocker/run.cmd
+++ Producers/dotnet-commits-producer/KafkaDocker/run.cmd
@@ -1,2 +1,2 @@
 docker build . -t commitproducer:latest 
-docker run -ti --rm -e ENABLE_INIT_DAEMON=false --network big-data-network --name commitproducer commitproducer
+docker run -ti --rm -e ENABLE_INIT_DAEMON=false -e DAAB_KAFKA_URL="10.123.252.230:9092,10.123.252.197:9092,10.123.252.206:9092" --network big-data-network --name commitproducer commitproducer
