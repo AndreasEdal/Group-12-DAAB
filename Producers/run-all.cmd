0e209b691081320fb5f52566766d85205aabc990
Andreas Edal Pedersen <anepe19@student.sdu.dk>
2022-12-13 13:56:45 +0100

    run all

--- Producers/run-all.cmd
+++ Producers/run-all.cmd
@@ -1,8 +1,8 @@
 docker build dotnet-commits-producer/KafkaDocker/. -t commitproducer:latest 
-docker run -ti --rm -e ENABLE_INIT_DAEMON=false --network big-data-network --name commitproducer commitproducer
+docker run -ti --rm -e ENABLE_INIT_DAEMON=false -e DAAB_KAFKA_URL="10.123.252.230:9092,10.123.252.197:9092,10.123.252.206:9092" --network big-data-network --name commitproducer commitproducer
 
 docker build dotnet-content-producer/KafkaDocker/. -t contentproducer:latest 
-docker run -ti --rm -e ENABLE_INIT_DAEMON=false --network big-data-network --name contentproducer contentproducer
+docker run -ti --rm -e ENABLE_INIT_DAEMON=false -e DAAB_KAFKA_URL="10.123.252.230:9092,10.123.252.197:9092,10.123.252.206:9092" --network big-data-network --name contentproducer contentproducer
 
 docker build dotnet-language-producer/KafkaDocker/. -t languageproducer:latest 
-docker run -ti --rm -e ENABLE_INIT_DAEMON=false --network big-data-network --name languageproducer languageproducer
+docker run -ti --rm -e ENABLE_INIT_DAEMON=false -e DAAB_KAFKA_URL="10.123.252.230:9092,10.123.252.197:9092,10.123.252.206:9092" --network big-data-network --name languageproducer languageproducer
